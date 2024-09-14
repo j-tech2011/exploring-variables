8d5f2ac63375fabcb7ec1db6afb1951c4caa3c79
James McCauley <jamesmccauley@ESA-1.local>
2024-09-14 12:58:26 +0100

    profile created with input

---
+++ groupchallange.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+age = int(input("what is your age?"))
+height = float(input("what is your height?"))
+
+print("Your name is: ", name, "and your height is: ", height, "and your age is: ", age)
